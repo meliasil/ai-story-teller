133a40da59f0b069b55da0d73affdc1799424bb1
Your Name <you@example.com>
2024-09-08 12:05:44 +0200

    feat: add error handler

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,13 +1,35 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+//Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
 
 type Data = {
   name: string;
 };
 
+const risposta = {
+  "userId": 1,
+  "id": 1,
+  "title": "delectus aut autem",
+  "completed": false
+  }
+
 export default function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>,
+  res: NextApiResponse<any>,
 ) {
-  res.status(200).json({ name: "John Doe" });
+
+  
+
+  if (req.method === "GET") {
+    res.status(200).json(risposta);
+  } else if (req.method === "POST") {
+    const {saluto} = JSON.parse(req.body)
+    if (saluto) {
+      res.status(200).json("HELLO!!!!");
+    } else {
+      res.status(404).json("Manca saluto");
+    }
+  } else {
+    res.status(400).json("Metodo non gestito");
+  }
+
 }
